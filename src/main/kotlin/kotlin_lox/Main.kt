778d7789b892ba4a07e76e3e9f29e72306754796
Gabe <15304068+gabesaba@users.noreply.github.com>
2022-12-21 16:13:15 -0700

    ktformat

--- src/main/kotlin/kotlin_lox/Main.kt
+++ src/main/kotlin/kotlin_lox/Main.kt
@@ -8,53 +8,53 @@ import kotlin.system.exitProcess
 var hadError = false
 
 fun main(args: Array<String>) {
-    when (args.size) {
-        0 -> runPrompt()
-        1 -> interpretFile(args[0])
-        else -> {
-            println("Usage: klox [script]")
-            exitProcess(64)
-        }
+  when (args.size) {
+    0 -> runPrompt()
+    1 -> interpretFile(args[0])
+    else -> {
+      println("Usage: klox [script]")
+      exitProcess(64)
     }
+  }
 }
 
 fun interpretFile(fileName: String) {
-    println("Handling $fileName")
-    val file = Path(fileName)
-    if (file.notExists()) {
-        println("File doesn't exist...")
-        exitProcess(1)
-    }
-    run(file.readText())
+  println("Handling $fileName")
+  val file = Path(fileName)
+  if (file.notExists()) {
+    println("File doesn't exist...")
+    exitProcess(1)
+  }
+  run(file.readText())
 
-    if (hadError) {
-        exitProcess(65)
-    }
+  if (hadError) {
+    exitProcess(65)
+  }
 }
 
 fun run(source: String): String {
 
-    val tokens = tokenize(source)
-    for (token in tokens) {
-        println(token)
-    }
-    return source
+  val tokens = tokenize(source)
+  for (token in tokens) {
+    println(token)
+  }
+  return source
 }
 
 fun runPrompt() {
-    while (true) {
-        print("-> ")
-        val line = readlnOrNull() ?: break
-        run(line)
-        hadError = false
-    }
+  while (true) {
+    print("-> ")
+    val line = readlnOrNull() ?: break
+    run(line)
+    hadError = false
+  }
 }
 
 fun error(line: Int, message: String) {
-    report(line, "", message)
+  report(line, "", message)
 }
 
 private fun report(line: Int, where: String, message: String) {
-    hadError = true
-    println("Error on line $line, $where: $message")
+  hadError = true
+  println("Error on line $line, $where: $message")
 }
