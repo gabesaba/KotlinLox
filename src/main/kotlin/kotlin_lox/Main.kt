7f0c73fcafcfc26670b5afa06e98ad04663b596a
Gabe <15304068+gabesaba@users.noreply.github.com>
2022-12-21 12:17:22 -0700

    Main, Errors, and Token

--- src/main/kotlin/kotlin_lox/Main.kt
+++ src/main/kotlin/kotlin_lox/Main.kt
@@ -1,5 +1,60 @@
 package kotlin_lox
 
-fun main() {
-    println("Hello, Gabe!")
+import kotlin.io.path.Path
+import kotlin.io.path.notExists
+import kotlin.io.path.readText
+import kotlin.system.exitProcess
+
+var hadError = false
+
+fun main(args: Array<String>) {
+    when (args.size) {
+        0 -> runPrompt()
+        1 -> interpretFile(args[0])
+        else -> {
+            println("Usage: klox [script]")
+            exitProcess(64)
+        }
+    }
+}
+
+fun interpretFile(fileName: String) {
+    println("Handling $fileName")
+    val file = Path(fileName)
+    if (file.notExists()) {
+        println("File doesn't exist...")
+        exitProcess(1)
+    }
+    run(file.readText())
+
+    if (hadError) {
+        exitProcess(65)
+    }
+}
+
+fun run(source: String): String {
+
+    val tokens = tokenize(source)
+    for (token in tokens) {
+        println(token)
+    }
+    return source
+}
+
+fun runPrompt() {
+    while (true) {
+        print("-> ")
+        val line = readlnOrNull() ?: break
+        run(line)
+        hadError = false
+    }
+}
+
+fun error(line: Int, message: String) {
+    report(line, "", message)
+}
+
+private fun report(line: Int, where: String, message: String) {
+    hadError = true
+    println("Error on line $line, $where: $message")
 }
---
+++ src/main/kotlin/kotlin_lox/Token.kt
@@ -0,0 +1,65 @@
+package kotlin_lox
+
+data class Token(val type: TokenType, val lexeme: String, val literal: Literal, val line: Int) {
+
+  override fun toString(): String {
+    return "$type $lexeme $literal"
+  }
+}
+
+class Literal
+
+enum class TokenType {
+  // Single-character tokens.
+  LEFT_PAREN,
+  RIGHT_PAREN,
+  LEFT_BRACE,
+  RIGHT_BRACE,
+  COMMA,
+  DOT,
+  MINUS,
+  PLUS,
+  SEMICOLON,
+  SLASH,
+  STAR,
+
+  // One or two-character tokens.
+  BANG,
+  BANG_EQUAL,
+  EQUAL,
+  EQUAL_EQUAL,
+  GREATER,
+  GREATER_EQUAL,
+  LESS,
+  LESS_EQUAL,
+
+  // Literals.
+  IDENTIFIER,
+  STRING,
+  NUMBER,
+
+  // Keywords.
+  AND,
+  CLASS,
+  ELSE,
+  FALSE,
+  FUN,
+  FOR,
+  IF,
+  NIL,
+  OR,
+  PRINT,
+  RETURN,
+  SUPER,
+  THIS,
+  TRUE,
+  VAR,
+  WHILE,
+
+  // Etc.
+  EOF
+}
+
+fun tokenize(source: String): List<Token> {
+  return emptyList()
+}
