58a47710d51b4bb8916bb808c2e41bdb956e65f7
Gabe <gabesaba@gmail.com>
2022-12-24 14:15:41 -0700

    Support enclosing environment

--- src/main/kotlin/kotlin_lox/Environment.kt
+++ src/main/kotlin/kotlin_lox/Environment.kt
@@ -1,21 +1,22 @@
 package kotlin_lox
 
-class Environment {
+class Environment(private val enclosing: Environment? = null) {
   private val values = mutableMapOf<String, Literal>()
 
   fun get(key: String): Literal? {
-    return values[key]
+    // Attempt to get key from environment, otherwise recursively from enclosing.
+    return values[key] ?: enclosing?.get(key)
   }
 
   fun define(key: String, value: Literal) {
     values[key] = value
   }
 
-  // Assign a value to a defined variable.
+  // Assign a value to a defined variable in the first enclosing scope.
   // Return true if successful, false if the variable hasn't been defined.
   fun assign(key: String, value: Literal): Boolean {
     if (key !in values) {
-      return false
+      return enclosing?.assign(key, value) ?: false
     }
     values[key] = value
     return true
